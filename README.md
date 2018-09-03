94643e8b06f070178fe08baf913123b6fcb24115
root <root@localhost.localdomain>
2018-09-03 15:58:54 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# TDThashID
+
+Hash identifier
+
+## Usage
+``` sh
+python TDThashID --hash HASH 
+```
+![Print](/images/Print.png)
+
+That's all folks!
+
+## License
+
+TDThashID is licensed under the GNU GPL license. take a look at the [LICENSE](https://github.com/TiagoANeves/TDTsubdomainFND/blob/master/LICENSE) for more information.
+
+## Version
+**Current version is 1.0.0**
+
+## Credits
+- [Tiago Neves](https://github.com/TiagoANeves)
---
+++ TDThashID.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#Developed by Tiago Neves
+#Github: https://github.com/TiagoANeves
+#Version: 1.0
+#All rights reserved
+
+#Import necessary modules
+import os
+import re
+import sys
+from argparse import ArgumentParser
+
+os.system("clear")
+
+#Color scheme
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Create Banner
+def banner():
+    print("""%s
+	 _______ _____ _______ _               _     _____ _____
+ 	|__   __|  __ \__   __| |             | |   |_   _|  __ \\
+    	   | |  | |  | | | |  | |__   __ _ ___| |__   | | | |  | |
+    	   | |  | |  | | | |  | '_ \ / _` / __| '_ \  | | | |  | |
+    	   | |  | |__| | | |  | | | | (_| \__ \ | | |_| |_| |__| |
+    	   |_|  |_____/  |_|  |_| |_|\__,_|___/_| |_|_____|_____/%s%s
+    # Coded By Tiago Neves
+    # Github https://github.com/TiagoANeves
+    """ % (bcolors.OKBLUE, bcolors.ENDC, bcolors.FAIL))
+
+# Hashes
+def hash_id(hash):
+    hash_types = []
+    # MD5
+    if re.match(r"^[a-fA-F0-9]{32}$", hash):
+	hash_types.append("MD5")
+    # SHA-1
+    if re.match(r"^[a-fA-F0-9]{40}$", hash):
+	hash_types.append("SHA-1")
+    # SHA-224
+    if re.match(r"^[a-fA-F0-9]{56}$", hash):
+        hash_types.append("SHA-224")
+    # SHA-256
+    if re.match(r"^[a-fA-F0-9]{64}$", hash):
+        hash_types.append("SHA-256")
+    # SHA-384
+    if re.match(r"^[a-fA-F0-9]{96}$", hash):
+        hash_types.append("SHA-384")
+    # SHA-512
+    if re.match(r"^[a-fA-F0-9]{128}$", hash):
+        hash_types.append("SHA-512")
+    return hash_types 
+
+# Arguments Parser
+parser = ArgumentParser()
+parser.add_argument("--hash", help="hash to be identified %s" %bcolors.ENDC)
+
+# Main function
+if __name__ == "__main__":
+    try:
+        banner()
+        print "This program will try to identify the different types of hashes \n" + bcolors.ENDC
+        args = parser.parse_args()
+    except:
+        sys.exit(0)
+    if len(sys.argv) != 3:
+        print bcolors.WARNING + "Usage: python "+sys.argv[0]+" --hash hash\n" + bcolors.ENDC
+        print bcolors.WARNING + "Use "+sys.argv[0]+" -h or --help to print the help option\n" + bcolors.ENDC
+        sys.exit()
+    else:
+        print bcolors.HEADER + "\nStarting the program...\n" + bcolors.ENDC
+	try:
+            results = hash_id(args.hash)
+            results_len = len(results)
+            #print results_len
+            if results_len >= 0:
+                print bcolors.WARNING + "Possible hashs Algorithms:" + bcolors.ENDC
+                for result in results:
+                    print "%s[+] %s%s%s\n" %(bcolors.OKGREEN,bcolors.OKBLUE,result,bcolors.ENDC)
+            else:
+	        print bcolors.FAIL + "The hash could not be identified...\n" + bcolors.ENDC
+        except:
+	    print "Error checking the hash"
+            sys.exit()
